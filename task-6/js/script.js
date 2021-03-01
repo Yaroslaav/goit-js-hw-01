3e9a0b629bb9b96d627abff6cc1997c59db4e5ef
Yaroslaav <cahloyaroslav@gmail.com>
2021-03-01 20:07:15 +0200

    tas6

--- task-6/js/script.js
+++ task-6/js/script.js
@@ -4,8 +4,9 @@ let total1 = 0;
 let input = Number(prompt('Введіть число'));
 while(input){
 	total1 += input;
-	alert('Загальна сума чисел дорівнює ' + total1);
 	console.log('Загальна сума чисел дорівнює ' + total1);
 	input = Number(prompt('Введіть число'));
 };
 
+alert('Загальна сума чисел дорівнює ' + total1);
+console.log('Загальна сума чисел дорівнює ' + total1);
